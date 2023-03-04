0479d2baa874da1d2a066b327893b8eb82faa011
Himanshu Kumar Shahi <113952620+himanshushahi@users.noreply.github.com>
2023-03-04 13:53:15 +0530

    add the enter button functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let output = document.getElementById("output");
 let means = document.getElementById("means");
-document.getElementById("search").addEventListener("click", () => {
-  let word = document.getElementById("input").value;
-  output.innerText = "Please wait..."
+let searchBtn = document.getElementById("search");
+let inputField = document.getElementById("input");
+
+function searchWord() {
+  let word = inputField.value;
+  output.innerText = "Please wait...";
   let data = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
   data
     .then((response) => {
@@ -13,7 +16,16 @@ document.getElementById("search").addEventListener("click", () => {
       }
     })
     .then((responce) => {
-      means.innerHTML = `<h4>Result for:</h4><p style="text-transform: capitalize; color:green">${word}</p>`
+      means.innerHTML = `<h4>Result for:</h4><p style="text-transform: capitalize; color:green">${word}</p>`;
       output.innerText = responce[0].meanings[0].definitions[0].definition;
     });
+}
+
+searchBtn.addEventListener("click", searchWord);
+
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    searchWord();
+  }
 });
